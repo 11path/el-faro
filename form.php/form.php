183d41df3b9e3d4ec0f61f7a4d0cd8f2eaecadd8
11path <164713955+11path@users.noreply.github.com>
2024-04-30 19:01:30 -0400

    Add files via upload

--- form.php/form.php
+++ form.php/form.php
@@ -6,7 +6,7 @@ class Cliente {
     }
 }
 
-
+$mensaje='';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST['nombre'];
     $email = $_POST['email'];
@@ -17,59 +17,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $registro_exitoso = $cliente->registrarCliente($nombre, $email, $plan);
 
     if ($registro_exitoso) {
-        // Redireccionar a la página de agradecimiento después de 2 segundos
-        /*entre las comillas despues de url=va la direccion de la pagina de agradecimiento*/
-        header("refresh:2;url=https://11path.github.io/universidad/");
-        exit();
+        $mensaje = gracias por registrarte! Nos pondremos en contacto contigo 
     }
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario de Registro</title>
-    
-</head>
-<body>
-    
-    <form action="" method="post">
-        <h2>Suscripción al diario online</h2>
-        <label for="nombre">Nombre:</label><br>
-        <input type="text" id="nombre" name="nombre" required><br><br>
-        <label for="email">Email:</label><br>
-        <input type="email" id="email" name="email" required><br><br>
-        <label for="plan">Plan de suscripción:</label><br>
-        <select id="plan" name="plan" required>
-            <option value="FREE">FREE</option>
-            <option value="BASICO">BÁSICO</option>
-            <option value="PREMIUM">PREMIUM</option>
-        </select><br><br>
-        <input type="submit" value="Registrarse">
-    </form>
-
-    <!-- Mensaje de agradecimiento -->
-    <div id="agradecimiento" style="display: none;">
-        <h2>¡Gracias por registrarte!</h2>
-        <p>Redirigiendo de vuelta...</p>
-    </div>
-
-    <script>
-        // Script para ocultar el formulario y mostrar el mensaje de agradecimiento después de enviar el formulario
-        const formulario = document.querySelector('form');
-        const mensajeAgradecimiento = document.getElementById('agradecimiento');
-
-        formulario.addEventListener('submit', function(event) {
-            event.preventDefault();
-            formulario.style.display = 'none';
-            mensajeAgradecimiento.style.display = 'block';
-            setTimeout(function() {
-                window.location.href = 'https://11path.github.io/el-faro/'; 
-            }, 2000); 
-        });
-    </script>
-</body>
-</html>
